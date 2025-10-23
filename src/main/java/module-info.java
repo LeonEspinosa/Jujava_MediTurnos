63bfb45fdeca090f3239a776030c1ee8589b4051
LeonEspinosa <francoespinosaleon@gmail.com>
2025-10-22 23:34:23 -0300

    cambio de module y pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,21 @@
-module jujava.mediturnos {
+module com.example.myjavafx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
-    opens jujava.mediturnos to javafx.fxml;
-    exports jujava.mediturnos;
+    // 🔹 Abre los paquetes necesarios para FXML y controladores
+    opens com.example.myjavafx to javafx.fxml;
+    opens com.example.myjavafx.view to javafx.fxml;
+    opens com.example.myjavafx.controller to javafx.fxml;
+    opens com.example.myjavafx.model to javafx.base; // solo este usa javafx.base
+
+    // 🔹 Exporta los paquetes que otros módulos pueden usar
+    exports com.example.myjavafx;
+    exports com.example.myjavafx.view;
+    exports com.example.myjavafx.controller;
+    exports com.example.myjavafx.model;
 }
